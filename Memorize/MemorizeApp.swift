5ca3a8eedda02c4177984c34affb69d00ac13dff
Eduardo Ximenes <e.ximenes17@hotmail.com>
2022-07-23 09:26:47 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Eduardo Ximenes on 23/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
