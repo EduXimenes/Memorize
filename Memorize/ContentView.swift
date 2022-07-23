ce6a30e1a10a1acbf29bf37156634c63c3b1f00f
Eduardo Ximenes <e.ximenes17@hotmail.com>
2022-07-23 09:38:03 -0300

    First Commit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -2,20 +2,61 @@
 //  ContentView.swift
 //  Memorize
 //
-//  Created by Eduardo Ximenes on 23/07/22.
+//  Created by Eduardo Ximenes on 21/07/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+         HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding(.horizontal)
+        .foregroundColor(.orange)
+        
+        
+
     }
 }
+struct CardView: View{
+    var isFaceUp: Bool = false
+    
+    
+    var body: some View{
+        ZStack{
+            let shape  = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+            shape.fill().foregroundColor(.white)
+            shape.stroke(lineWidth: 3)
+            Text("🎃").font(.largeTitle)
+            }
+            else {
+                shape.fill()
+            }
+    }
+    }}
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            ContentView()
+                .preferredColorScheme(.light)
     }
 }
