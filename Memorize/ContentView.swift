3d79b282f6733a4fc0e2949bd8b5cb4c6a3d4130
Eduardo Ximenes <e.ximenes17@hotmail.com>
2022-07-23 19:53:39 -0300

    Adding adding grid to cards and buttons to add and remove

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,47 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis: Array<String> = ["🦖", "🐥", "🪲", "🐔","🐸", "🐵", "🐧", "🦆", "🦉", "🐝", "🐙", "🐼","🐶", "🐹", "🦇", "🐌", "🐜", "🦐", "🐖", "🦥", "🐞", "🐨",
+        "🦁", "🦗", "🕷", "🦂", "🐳", "🦒", "🦧", "🐲", "🦩", "🕊", "🐕", "🦞",]
+    // Podemos também declarar como : [String] que seria mesma coisa que Array<String>
+    @State var emojiCount = 4
+    
     var body: some View {
-         HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack{
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+                             ForEach(emojis[0..<emojiCount], id: \.self){emoji in
+                                 CardView(content: emoji)
+                                     .aspectRatio(2/3, contentMode: .fit)
+                             }
+                        }
+            }
+            Spacer()
+            HStack{
+                    removeCard
+                    Spacer()
+                    addCard
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
         .foregroundColor(.orange)
-        
-        
-
+    }
+    var removeCard: some View{
+        Button {
+            if emojiCount > 2{
+                emojiCount-=1
+            }
+        } label: {
+            Image(systemName: "minus.square")
+            }
+        }
+    var addCard: some View{
+        Button {
+            if emojiCount < emojis.count{
+                emojiCount+=1
+            }
+        } label: {
+                Image(systemName: "plus.square")
+            }
     }
 }
 struct CardView: View{
-    var isFaceUp: Bool = false
-    
+    var content: String
+    @State var isFaceUp: Bool = true
     
     var body: some View{
         ZStack{
             let shape  = RoundedRectangle(cornerRadius: 20)
             if isFaceUp {
             shape.fill().foregroundColor(.white)
-            shape.stroke(lineWidth: 3)
-            Text("🎃").font(.largeTitle)
+            shape.strokeBorder(lineWidth: 5)
+            Text(content).font(.largeTitle)
             }
             else {
                 shape.fill()
             }
+        }
+        .onTapGesture{
+            isFaceUp = !isFaceUp
+        }
     }
-    }}
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
